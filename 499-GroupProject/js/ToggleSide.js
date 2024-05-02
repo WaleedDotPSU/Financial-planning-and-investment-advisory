bdaea9f71c7678e322aefd39a99f0dc046760226
WaleedDotPSU <219110660@psu.edu.sa>
2024-05-02 09:56:09 +0300

    improved dashbored and slide bar

--- 499-GroupProject/js/ToggleSide.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.expand-menu').addEventListener('click', function() {
-  document.querySelector('.sidebar').style.width = '250px';
-});
-
-document.querySelector('.sidebar').addEventListener('click', function() {
-  this.style.width = '0';
-});
