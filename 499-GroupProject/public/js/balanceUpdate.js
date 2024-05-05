6ae8254512cf782cbc88cce20946a0e6f5f7a4fb
mohabus <158074388+mohabus@users.noreply.github.com>
2024-05-05 11:55:32 +0300

    Update WIthdraw functionality
    - The withdraw functionality now actullay withdraw money from the wallet balance, and checks if the balance have enough amount of money.
    
    - Put the deposit and withdraw functions into one js file

--- 499-GroupProject/public/js/balanceUpdate.js
+++ 499-GroupProject/public/js/balanceUpdate.js
@@ -1,6 +1,3 @@
-// THIS FILE SHOULD BE DELETED, because now we have the balanceUpdate.js
-// which handles the deposit and the withdraw functions
-
 var g_walletBalance = parseFloat(localStorage.getItem("g_walletBalance")) || 0.0;
 
 function updateBalance() {
@@ -12,6 +9,19 @@ function updateBalance() {
 
     let balanceElement = document.getElementById("walletBalance");
     balanceElement.innerText = "Wallet Balance: " + g_walletBalance.toFixed(2) + " ريال";
+}
+
+function withdrawMoney() {
+    let withdrawAmount = parseFloat(document.getElementById("amount").value);
+
+    if (withdrawAmount <= g_walletBalance) {
+        g_walletBalance -= withdrawAmount;
+        localStorage.setItem("g_walletBalance", g_walletBalance);
+        document.getElementById("walletBalance").innerText = "Wallet Balance: " + g_walletBalance.toFixed(2) + " ريال";
+        alert("Withdrawal successful!");
+    } else {
+        alert("Insufficient funds.");
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
